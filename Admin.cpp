227ac920d4aaf66c58a06f3ca4f501d9da86f962
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 14:39:03 -0500

    Create Admin.cpp

---
+++ Admin.cpp
@@ -0,0 +1,21 @@
+/*
+ * Admin.cpp
+ *
+ *  Created on: Feb 1, 2014
+ *      Author: nzayatz14
+ */
+
+#include "Admin.h"
+
+namespace std {
+
+Admin::Admin() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Admin::~Admin() {
+	// TODO Auto-generated destructor stub
+}
+
+}
