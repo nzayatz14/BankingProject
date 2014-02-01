fab2237a29b738ab9eedbc0b8e7cb3a55ebe25da
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 14:40:46 -0500

    Create Person.h

---
+++ Person.h
@@ -0,0 +1,21 @@
+/*
+ * Person.h
+ *
+ *  Created on: Feb 1, 2014
+ *      Author: nzayatz14
+ */
+
+#ifndef PERSON_H_
+#define PERSON_H_
+
+namespace std {
+
+class Person {
+public:
+	Person();
+	virtual ~Person();
+};
+
+}
+
+#endif /* PERSON_H_ */
