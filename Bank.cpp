08bcc144b84119e33c878fd74009b3d2264ebb14
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 14:32:26 -0500

    Create Bank.cpp

---
+++ Bank.cpp
@@ -0,0 +1,17 @@
+/*
+ * Bank.cpp
+ *
+ *  Created on: Feb 1, 2014
+ *      Author: nzayatz14
+ */
+
+#include "Bank.h"
+using namespace std;
+Bank::Bank() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Bank::~Bank() {
+	// TODO Auto-generated destructor stub
+}
