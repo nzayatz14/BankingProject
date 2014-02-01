8e88703e46a843aafd11981a4467d5651272dea8
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 14:29:04 -0500

    Create BankingProject.cpp

---
+++ BankingProject.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : BankingProject.cpp
+// Author      : Nick Zayatz, Junjie Liao, Chris Davila, Michael Cantor
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Main class for BankingProject
+//============================================================================
+
+#include <iostream>
+#include "Bank.h"
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
