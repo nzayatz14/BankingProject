b4a1081236f76be06b72cec587378e151702ee83
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 14:39:33 -0500

    Create Client.cpp

---
+++ Client.cpp
@@ -0,0 +1,21 @@
+/*
+ * Client.cpp
+ *
+ *  Created on: Feb 1, 2014
+ *      Author: nzayatz14
+ */
+
+#include "Client.h"
+
+namespace std {
+
+Client::Client() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Client::~Client() {
+	// TODO Auto-generated destructor stub
+}
+
+}
