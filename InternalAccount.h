8640c7973e79a52d250516e0ad85bd7071a3c55e
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 14:37:55 -0500

    Create InternalAccount.h

---
+++ InternalAccount.h
@@ -0,0 +1,21 @@
+/*
+ * InternalAccount.h
+ *
+ *  Created on: Feb 1, 2014
+ *      Author: nzayatz14
+ */
+
+#ifndef INTERNALACCOUNT_H_
+#define INTERNALACCOUNT_H_
+
+namespace std {
+
+class InternalAccount {
+public:
+	InternalAccount();
+	virtual ~InternalAccount();
+};
+
+}
+
+#endif /* INTERNALACCOUNT_H_ */
