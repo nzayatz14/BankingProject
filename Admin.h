9cb90cf86fc7ee16cbb8e38833e321c4a7b69641
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 14:38:43 -0500

    Create Admin.h

---
+++ Admin.h
@@ -0,0 +1,21 @@
+/*
+ * Admin.h
+ *
+ *  Created on: Feb 1, 2014
+ *      Author: nzayatz14
+ */
+
+#ifndef ADMIN_H_
+#define ADMIN_H_
+
+namespace std {
+
+class Admin {
+public:
+	Admin();
+	virtual ~Admin();
+};
+
+}
+
+#endif /* ADMIN_H_ */
