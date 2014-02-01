7037b0c0bdaf7c84f981532377871cdd9f0e30ea
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 16:59:37 -0500

    Update ExternalAccount.cpp

--- ExternalAccount.cpp
+++ ExternalAccount.cpp
@@ -10,12 +10,64 @@
 namespace std {
 
 ExternalAccount::ExternalAccount() {
-	// TODO Auto-generated constructor stub
+	accountNumber = bank.getTotalAccounts()+1;
+	accountHolder = (new Client());
+	password = "password";
 
 }
 
+void ExternalAccount::createInternalAccounts(){
+		internalAccounts[0].setAccountType("Checking");
+		internalAccounts[0].setExternalAccount(this);
+		internalAccounts[0].setMoney(0);
+		internalAccounts[1].setMoney(0);
+		internalAccounts[1].setAccountType("Savings");
+		internalAccounts[1].setExternalAccount(this);
+}
+
+void ExternalAccount::displayExternalAccount(){
+	cout<<accountHolder.getUserName()<<" "<<password<<" "<<accountNumber<<endl;
+}
+
+int ExternalAccount::getPassword(){
+	return password;
+}
+
+void ExternalAccount::getAccountHolder(Client &c){
+	accountHolder.copyClient(c);
+}
+
+void ExternalAccount::getInternalAccounts(InternalAccount &d[]){
+	internalAccounts[0].copyInternalAccount(d[0]);
+	internalAccounts[1].copyInternalAccount(d[1]);
+}
+
+void ExternalAccount::setPassword(string s){
+	password = s;
+}
+
+void ExternalAccount::setAccountNumber(int g){
+	accountNumber = g;
+}
+
+void ExternalAccount::setInternalAccounts(InternalAccount &a, InternalAccount &b){
+	a.copyInternalAccount(internalAccount[0]);
+	b.copyInternalAccount(internalAccount[1]);
+}
+
+void ExternalAccount::setAccountHolder(Client &c){
+	c.copyClient(accountHolder);
+}
+
+void ExternalAccount::copyExternalAccount(ExternalAccount &e){
+	e.setAccountNumber(accountNumber);
+	e.setAccountHolder(accountHolder);
+	e.setInternalAccounts(internalAccounts[0], internalAccounts[1]);
+	e.setPassword(password);
+}
+
 ExternalAccount::~ExternalAccount() {
-	// TODO Auto-generated destructor stub
+	delete [] accountNumber;
 }
 
 }
