a360b49655e39ddc163117aaa2b93e43dd481534
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 17:00:08 -0500

    Update ExternalAccount.h

--- ExternalAccount.h
+++ ExternalAccount.h
@@ -10,18 +10,6 @@
 
 namespace std {
 
-class ExternalAccount;
-
-template<>
-class hash<ExternalAccount>{
-	
-	result_type operator()(const ExternalAccount &c) const{
-		size_t value{0};
-		boost::hash_combine(value, )
-	}
-}
-
-
 class ExternalAccount {
 private:
 	int accountNumber;
@@ -29,10 +17,36 @@ private:
 	InternalAccount internalAccounts[2];
 	string password;
 public:
+	int getAccountNumber();
+	string getPassword();
+	void getAccountHolder(Client &c);
+	void getInternalAccounts(InternalAccount &d[]);
+
+	void setPassword(string s);
+	void setAccountHolder(Client &c);
+	void setInternalAccounts(InternalAccount &a[]);
+	void setAccountNumber(int g);
+
+	void createInternalAccount(string name);
+	void displayExternalAccount();
+	void copyExternalAccount(ExternalAccount &e);
+
 	ExternalAccount();
 	virtual ~ExternalAccount();
 };
 
+template<>
+class hash<ExternalAccount>{
+	typedef size_t result_type;
+
+	result_type operator()(const ExternalAccount &c) const{
+		size_t value{0};
+		boost::hash_combine(value, c.accountNumber);
+		boost::hash_combine(value, c.password);
+
+		return value;
+	}
+}
 }
 
 #endif /* EXTERNALACCOUNT_H_ */
