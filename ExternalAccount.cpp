df453de76651ffa489988d07305dda0aa27891e9
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 14:31:41 -0500

    Create ExternalAccount.cpp

---
+++ ExternalAccount.cpp
@@ -0,0 +1,21 @@
+/*
+ * ExternalAccount.cpp
+ *
+ *  Created on: Feb 1, 2014
+ *      Author: nzayatz14
+ */
+
+#include "ExternalAccount.h"
+
+namespace std {
+
+ExternalAccount::ExternalAccount() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ExternalAccount::~ExternalAccount() {
+	// TODO Auto-generated destructor stub
+}
+
+}
