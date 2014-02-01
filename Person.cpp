76e522c0e12a34f6a8a6ed82cf075ddfbd82f3cf
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 14:40:27 -0500

    Create Person.cpp

---
+++ Person.cpp
@@ -0,0 +1,21 @@
+/*
+ * Person.cpp
+ *
+ *  Created on: Feb 1, 2014
+ *      Author: nzayatz14
+ */
+
+#include "Person.h"
+
+namespace std {
+
+Person::Person() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Person::~Person() {
+	// TODO Auto-generated destructor stub
+}
+
+}
