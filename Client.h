dbae863f12009345198abaab108bddbd0a681040
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 14:39:51 -0500

    Create Client.h

---
+++ Client.h
@@ -0,0 +1,21 @@
+/*
+ * Client.h
+ *
+ *  Created on: Feb 1, 2014
+ *      Author: nzayatz14
+ */
+
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+namespace std {
+
+class Client {
+public:
+	Client();
+	virtual ~Client();
+};
+
+}
+
+#endif /* CLIENT_H_ */
