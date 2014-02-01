52b6c1f4f87aa6d5dee4eccf834116e11c3ec7e6
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 16:58:43 -0500

    Update Bank.h

--- Bank.h
+++ Bank.h
@@ -7,6 +7,7 @@
 
 #include<iostream>
 #include<unordered_set>
+#include "ExternalAccount.h"
 
 #ifndef BANK_H_
 #define BANK_H_
@@ -17,9 +18,19 @@ typedef unordered_set<ExternalAccount> Accounts;
 class Bank {
 private:
 	Accounts bank;
+	int totalAccounts;
 public:
 	Bank();
 	virtual ~Bank();
+
+	int getTotalAccounts();
+	void setTotalAccounts(int g);
+
+	void printBank();
+	void createExternalAccount(Client &a, string pass);
+	void deleteExternalAccount(int a);
+	void login(ExternalAccount &a);
+	void logout(ExternalAccount &a);
 };
 
 #endif /* BANK_H_ */
