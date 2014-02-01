841be5dd201a8f429a1b6892e42b7e19ff409e0d
nzayatz14 <nzayatz14@hotmail.com>
2014-02-01 14:38:18 -0500

    Create InternalAccount.cpp

---
+++ InternalAccount.cpp
@@ -0,0 +1,21 @@
+/*
+ * InternalAccount.cpp
+ *
+ *  Created on: Feb 1, 2014
+ *      Author: nzayatz14
+ */
+
+#include "InternalAccount.h"
+
+namespace std {
+
+InternalAccount::InternalAccount() {
+	// TODO Auto-generated constructor stub
+
+}
+
+InternalAccount::~InternalAccount() {
+	// TODO Auto-generated destructor stub
+}
+
+}
