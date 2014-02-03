339dd01f7066b3558a5bf6742375309a2a9e9e4e
liaoj12 <jackliao2012@gmail.com>
2014-02-03 16:48:35 -0500

    Update InternalAccount.h

--- InternalAccount.h
+++ InternalAccount.h
@@ -2,20 +2,37 @@
  * InternalAccount.h
  *
  *  Created on: Feb 1, 2014
- *      Author: nzayatz14
+ *      Author: Junjieliao
  */
 
 #ifndef INTERNALACCOUNT_H_
 #define INTERNALACCOUNT_H_
 
-namespace std {
+#include <string>
+#include "ExternalAccount.h"
+
+using namespace std;
 
 class InternalAccount {
 public:
-	InternalAccount();
-	virtual ~InternalAccount();
+	InternalAccount(string,ExternalAccount&);	//constructor
+	virtual ~InternalAccount();	//destructor
+	void copyInternalAccount(InternalAccount& other);//function copies the information from an InternalAccount
+	void setAccountType(string a);	//function sets the account type
+	void setMoney(double m);	//function sets the money
+	void setExternalAccount(ExternalAccount m);	//function sets the corresponding external account
+	string getAccountType();	//function gets the type of the account
+	double getMoney();	//function gets the money in the account
+	ExternalAccount getExternalAccount();	//function gets the corresponding external account
+	void displayInternalAccount();	//function prints all information of the internal account
+
+private:
+	string accountType; //variable holds the type of the account
+	double money;		//variable holds the money in the account
+	ExternalAccount &externalAccount;	//variable holds the corresponding external account
 };
 
-}
+
+
 
 #endif /* INTERNALACCOUNT_H_ */
