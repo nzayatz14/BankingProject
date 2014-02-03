3e904e9d542585e1440e0a6f5d85a7931a8eabe7
liaoj12 <jackliao2012@gmail.com>
2014-02-03 16:49:30 -0500

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -1,13 +1,15 @@
+
 /*
  * Client.cpp
  *
  *  Created on: Feb 1, 2014
- *      Author: nzayatz14
+ *      Author: Junjieliao
  */
 
+#include<iostream>
 #include "Client.h"
 
-namespace std {
+using namespace std;
 
 Client::Client() {
 	// TODO Auto-generated constructor stub
@@ -18,4 +20,44 @@ Client::~Client() {
 	// TODO Auto-generated destructor stub
 }
 
+void Client::viewExternalAccount(){
+	ExternalAccount::displayExternalAccount();
 }
+
+void Client::withdraw(){
+	cout<<"Please enter the account name you wish to withdraw money"<<
+			"from:";
+	string acc;
+	cin>>acc;
+
+}
+void Client::deposit(){
+	cout<<"Please enter the account you want to deposit into:";
+	string acc;
+	cin>>acc;
+	cout<<"How much do you want to deposit into the account:";
+	double m;
+	cin>>m;
+
+
+}
+void Client::transfer(){
+	cout<<"Please enter the account you want to transfer from:"
+	string acc;
+	cin>>acc;
+	cout<<"How much do you want to transfer with:";
+	double m;
+	cin>>m;
+
+
+}
+void Client::printOptions(){
+	cout<<"Here you can do: 1.Withdrawl 2.deposit 3.transfer,"<<
+			"enter the number to keep running: ";
+	int n;
+	cin>>n;
+	switch(n):
+
+}
+
+
