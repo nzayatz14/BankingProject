b5db1e7bcfc688f02445f47362715ea6edf9220c
liaoj12 <jackliao2012@gmail.com>
2014-02-04 17:53:54 -0500

    Update InternalAccount.h

--- InternalAccount.h
+++ InternalAccount.h
@@ -15,12 +15,12 @@ using namespace std;
 
 class InternalAccount {
 public:
-	InternalAccount(string,ExternalAccount&);	//constructor
+	InternalAccount();	//constructor
 	virtual ~InternalAccount();	//destructor
 	void copyInternalAccount(InternalAccount& other);//function copies the information from an InternalAccount
-	void setAccountType(string a);	//function sets the account type
+	void setAccountType(string);	//function sets the account type
 	void setMoney(double m);	//function sets the money
-	void setExternalAccount(ExternalAccount m);	//function sets the corresponding external account
+	void setExternalAccount(ExternalAccount&);	//function sets the corresponding external account
 	string getAccountType();	//function gets the type of the account
 	double getMoney();	//function gets the money in the account
 	ExternalAccount getExternalAccount();	//function gets the corresponding external account
@@ -29,7 +29,7 @@ public:
 private:
 	string accountType; //variable holds the type of the account
 	double money;		//variable holds the money in the account
-	ExternalAccount &externalAccount;	//variable holds the corresponding external account
+	ExternalAccount externalAccount;	//variable holds the corresponding external account
 };
 
 
