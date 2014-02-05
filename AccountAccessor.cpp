4f35aaf0ac7aa72b721ce77125bd96a414aff759
nzayatz14 <nzayatz14@hotmail.com>
2014-02-05 17:10:35 -0500

    Create AccountAccessor.cpp

---
+++ AccountAccessor.cpp
@@ -0,0 +1,21 @@
+/*
+ * AccountAccessor.cpp
+ *
+ *  Created on: Feb 5, 2014
+ *      Author: nzayatz14
+ */
+
+#include "AccountAccessor.h"
+
+namespace std {
+
+AccountAccessor::AccountAccessor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AccountAccessor::~AccountAccessor() {
+	// TODO Auto-generated destructor stub
+}
+
+}
