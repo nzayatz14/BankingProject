f84f422de345877d9f753d8d892369b2ecd43ef3
nzayatz14 <nzayatz14@hotmail.com>
2014-02-05 17:08:42 -0500

    Update ExternalAccount.h

--- ExternalAccount.h
+++ ExternalAccount.h
@@ -5,24 +5,30 @@
  *      Author: nzayatz14
  */
 
+
+#include <iostream>
 #ifndef EXTERNALACCOUNT_H_
 #define EXTERNALACCOUNT_H_
+
+class Bank;
+class Client;
+class InternalAccount;
 
 namespace std {
 
 
 class ExternalAccount {
 private:
-	Bank b;
+	Bank* b;
 	int accountNumber;
-	Client accountHolder;
-	InternalAccount internalAccounts[2];
+	Client* accountHolder;
+	InternalAccount* internalAccounts[2];
 	string password;
 public:
 	int getAccountNumber();
 	string getPassword();
 	void getAccountHolder(Client &c);
-	void getInternalAccounts(InternalAccount &d[]);
+	void getInternalAccounts(InternalAccount &d, InternalAccount &e);
 
 	void setPassword(string s);
 	void setAccountHolder(Client &c);
