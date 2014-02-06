0532e12040983b321ff0dda1dc11add88f1b42da
mac2nd <cantom12@highpoint.edu>
2014-02-06 16:07:49 -0500

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -7,7 +7,7 @@
 
 #include "Admin.h"
 
-namespace std {
+namespace std;
 
 Admin::Admin() {
 	// TODO Auto-generated constructor stub
@@ -18,4 +18,46 @@ Admin::~Admin() {
 	// TODO Auto-generated destructor stub
 }
 
+void viewBank()
+{
+	return bank.printBank();
 }
+
+void viewAccounts()
+{
+	return bank.printTotal();
+}
+
+void viewAccountInDetail()
+{
+	// ask for the account number of the account that the user wishes to view
+	//if it is a valid account number, open<accountNumber.txt> read in all of
+	//the information and print it all. If its not print error and call printOptions
+
+}
+void createAccount()
+{
+	//ask for general information
+	//create client object from entered info
+	//call bank.create ExternalAccount(client,password) function for bank class
+	//call print options
+}
+
+void changePassword()
+{
+	//ask for the userName of the account (check to make sure it exists and is the //correct account)
+	//ask for new password and re-entry
+	//if both entries match, find the externalAccount in the bank and set password in
+	//the temporary account to the new password
+	//set the externalAccount in the Bank equal to the temporary
+	//call printOptions()
+}
+
+void printOptions()
+{
+	//print and number the capabilities of the administrator (create an account, delete
+	//an account, view a specific account, or change the password to an account)
+	//ask the option which the user wants to do (by number) and call the respective
+	//function
+}
+
