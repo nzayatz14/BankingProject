264483953bcc969d083b2faa679f86af754b8f73
nzayatz14 <nzayatz14@hotmail.com>
2014-02-06 15:11:36 -0500

    Update AccountAccessor.cpp

--- AccountAccessor.cpp
+++ AccountAccessor.cpp
@@ -6,7 +6,8 @@
  */
 
 #include "AccountAccessor.h"
-
+#include <fstream>
+#include <sstream>
 namespace std {
 
 AccountAccessor::AccountAccessor() {
@@ -18,4 +19,35 @@ AccountAccessor::~AccountAccessor() {
 	// TODO Auto-generated destructor stub
 }
 
+void AccountAccessor::login(Person &per){
+	string name, pass;
+	cout<<"Username: ";
+	cin>>name;
+	cout<<"Password: ";
+	cin>>pass;
+
+	ExternalAccount temp;
+
+	bank.find(name, temp);
+
+	if (pass.compare(temp.getPassword()) == 0){
+		int account = temp.getAccountNumber();
+
+		ifstream in;
+		ostringstream ss;
+		string accountNumb;
+		ss<<account;
+		accountNumb = ss.str();
+		accountNumb = accountNumb + ".txt";
+		in.open(accountNumb.c_str());
+
+		//read in data and set it to person
+
+		in.close();
+	}
+}
+
+void AccountAccessor::logout(){
+
+}
 }
