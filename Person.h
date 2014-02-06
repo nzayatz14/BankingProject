492a994117d8d50a1794a5b28d4ad4e675a5101b
mac2nd <cantom12@highpoint.edu>
2014-02-06 15:10:52 -0500

    Update Person.h

--- Person.h
+++ Person.h
@@ -14,8 +14,122 @@ class Person {
 public:
 	Person();
 	virtual ~Person();
+	void setName(string);
+	string getName();
+	void setBirthday(string);
+	string getBirthday();
+	void setGender(char);
+	string getGender();
+	void setPhoneNumber(string );
+	string getPhoneNumber();
+	void setAddress(string);
+	string getAddress();
+	void setEmail(string);
+	string getEmail();
+	void setUserName(string);
+	string getUserName();
+	void setPerson(string, string, char, string, string, string, string);
+	void displayPersonalInformation();
+
+	virtual void printOptions() = 0;
+
+private:
+	string name;
+	string birthday;
+	char gender;
+	string phoneNumber;
+	string address;
+	string email;
+	string userName;
 };
 
+void Person::setName(string n)
+{
+	name = n;
+}
+
+string Person::getName()
+{
+	return  name;
+}
+
+void Person::setBirthday(string b)
+{
+	birthday = b;
+}
+
+string Person::getBirthday()
+{
+	return birthday;
+}
+
+void Person::setGender(char g)
+{
+	gender = g;
+}
+
+string Person::getGender()
+{
+	return gender;
+}
+
+void Person::setPhoneNumber(string p)
+{
+	phoneNumber = p;
+}
+
+string Person::getPhoneNumber()
+{
+	return  phoneNumber;
+}
+
+void Person::setAddress(string a)
+{
+	address = a;
+}
+
+string Person::getAddress()
+{
+	return address;
+}
+
+void Person::setEmail(string e)
+{
+	email = e;
+}
+
+string Person::getEmail()
+{
+	return email;
+}
+
+string Person::getUserName()
+{
+	return userName;
+}
+
+void Person::setUserName(string un)
+{
+	userName = un;
+}
+
+void setPerson(string n, string b, char g, string p, string a, string e, string un)
+{
+
+}
+
+void displayPersonalInformation()
+{
+	/*string n;
+	n=Person.getName();
+	cout<<n;
+	*/
+}
+
+void printOptions()
+{
+
+}
 }
 
 #endif /* PERSON_H_ */
