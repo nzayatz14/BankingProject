acd38e86df3e01b721036c482b9f496977ae2e8a
liaoj12 <jackliao2012@gmail.com>
2014-02-25 12:38:20 -0500

    Update InternalAccount.h
    Update with comments

--- InternalAccount.h
+++ InternalAccount.h
@@ -14,24 +14,61 @@ using namespace std;
 
 class InternalAccount {
 public:
-	InternalAccount();	//constructor
-//	virtual ~InternalAccount();	//destructor
-	void copyInternalAccount(InternalAccount& other);//function copies the information from an InternalAccount
-	void setAccountType(string);	//function sets the account type
-	void setUsername(string);	//function sets the username
-	void setMoney(double m);	//function sets the money
-	string getAccountType();	//function gets the type of the account
-	double getMoney();	//function gets the money in the account
-	string getUsername();//function gets the username in the account
-	void displayInternalAccount();	//function prints all information of the internal account
+	InternalAccount();
+	//default constructor that sets
+	//accountType="Checking",
+	//money=0, and
+	//username="user"
+	//as default
+
+	void copyInternalAccount(InternalAccount& other);
+	//function copies the information from other InternalAccount variable
+	//Passes other InternalAccount variable as reference,
+	//copy the values of money, account type
+	//and username
+
+	void setAccountType(string);
+	//function sets the account type
+	//Set the value passed into the function
+	//into member: accountType
+
+	void setUsername(string);
+	//function sets the username
+	//Set the value passed into the function
+	//into member: username
+
+	void setMoney(double m);
+	//function sets the money
+	//Set the value passed into the function
+	//into member: money
+
+	string getAccountType();
+	//function gets the type of the account
+	//Return the value of member: accountType
+	//as string
+
+	double getMoney();
+	//function gets the money in the account
+	//Return the value of member: money
+	//as double
+
+	string getUsername();
+	//function gets the username in the account
+	//Return the value of member: username
+	//as string
+
+	void displayInternalAccount();
+	//function prints all information of the internal account
+	//Display the values of accountType and money
+	//in the format below:
+	//accontType:
+	//$money
 
 private:
 	string accountType; //variable to holds the type of the account
 	double money;		//variable to holds the money in the account
-	string username;	//variable to holds the username in the account
+	string username;	//variable to holds the user name in the account
 };
-
-
 
 
 #endif /* INTERNALACCOUNT_H_ */
